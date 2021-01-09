3e162fa6efcfe9e286b69c83979acd97fb204f83
Kristian Dunne <KristianDunne@icloud.com>
2021-01-09 14:22:14 +0000

    Use tailwind custom colours instead of css vars

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="flex flex-col h-screen px-5 pt-20 text-center text-white app">
+    <div className="flex flex-col h-screen px-5 py-20 text-center text-white app bg-primary-red">
       <div className="px-10 text-2xl font-bold">
         Learn to code by watching others
       </div>
@@ -11,7 +11,7 @@ function App() {
         scripted tutorials is great, but understanding how developers think is
         invaluable.
       </div>
-      <div className="py-3 text-sm leading-loose text-center rounded-lg shadow-xl mt-14 price-alert ">
+      <div className="py-3 text-sm leading-loose text-center rounded-lg shadow-xl mt-14 price-alert bg-accent-blue">
         <span className="text-sm font-bold">Try it free 7 days</span> then
         <br />
         $20/mo. thereafter
