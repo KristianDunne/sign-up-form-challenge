0e8c48fc4c442f303dc40d25e81ff995b14b6f1b
Kristian Dunne <KristianDunne@icloud.com>
2021-01-09 16:32:40 +0000

    Add focus styles to the forms input fields

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -2,25 +2,25 @@ export default function SignUpForm() {
   return (
     <form
       action="#"
-      className="flex flex-col p-0 p-5 mt-5 space-y-4 bg-white rounded-lg shadow-xl lg:p-10 lg:space-y-6"
+      className="flex flex-col p-0 p-5 mt-5 space-y-4 text-black bg-white rounded-lg shadow-xl lg:p-10 lg:space-y-6"
     >
       <input
-        className="p-4 font-semibold placeholder-gray-500 border rounded-lg lg:px-8"
+        className="p-4 font-semibold placeholder-gray-500 border rounded-lg outline-none lg:px-8 focus:ring-accent-blue focus:ring-1"
         placeholder="First Name"
         type="text"
       />
       <input
-        className="p-4 font-semibold placeholder-gray-500 border rounded-lg lg:px-8"
+        className="p-4 font-semibold placeholder-gray-500 border rounded-lg outline-none lg:px-8 focus:ring-accent-blue focus:ring-1"
         placeholder="Last Name"
         type="text"
       />
       <input
-        className="p-4 font-semibold placeholder-gray-500 border rounded-lg lg:px-8"
+        className="p-4 font-semibold placeholder-gray-500 border rounded-lg outline-none lg:px-8 focus:ring-accent-blue focus:ring-1"
         placeholder="Email Address"
         type="email"
       />
       <input
-        className="p-4 font-semibold placeholder-gray-500 border rounded-lg lg:px-8"
+        className="p-4 font-semibold placeholder-gray-500 border rounded-lg outline-none lg:px-8 focus:ring-accent-blue focus:ring-1"
         placeholder="Password"
         type="Password"
       />
