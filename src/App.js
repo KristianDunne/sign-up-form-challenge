bccb5a1b540723de5e2e8dcd7ee4652d803f3911
Kristian Dunne <KristianDunne@icloud.com>
2021-01-09 13:10:07 +0000

    Add the introduction text

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 
 function App() {
-  return <div className="flex flex-col h-screen px-5 pt-20 app" />;
+  return (
+    <div className="flex flex-col h-screen px-5 pt-20 text-center text-white app">
+      <div className="px-10 text-2xl font-bold">
+        Learn to code by watching others
+      </div>
+      <div className="mt-5 text-sm">
+        See how experienced developers solve problems in real-time. Watching
+        scripted tutorials is great, but understanding how developers think is
+        invaluable.
+      </div>
+    </div>
+  );
 }
 
 export default App;
