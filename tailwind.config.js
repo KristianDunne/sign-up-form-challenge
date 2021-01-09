fc0d8a666ed66f4fec8f5849d111e55cb2820ee4
Kristian Dunne <KristianDunne@icloud.com>
2021-01-09 14:21:59 +0000

    Extend tailwind colours to use custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-red': 'hsl(0, 100%, 74%)',
+        'primary-green': 'hsl(154, 59%, 51%)',
+        'accent-blue': 'hsl(248, 32%, 49%)',
+        'neutral-dark-blue': 'hsl(249, 10%, 26%)',
+        'neutral-greyish-blue': 'hsl(246, 25%, 77%)',
+      },
+    },
   },
   variants: {
     extend: {},
