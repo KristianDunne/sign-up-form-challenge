9d0c3c4c5404ca18e9a736eca9f8db92b6523edb
Kristian Dunne <KristianDunne@icloud.com>
2021-01-09 12:15:51 +0000

    Setup prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,16 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ["plugin:react/recommended", "airbnb"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ["react", "prettier"],
   rules: {
+    "prettier/prettier": "error",
   },
 };
