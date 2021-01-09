cc8a4c10b445af165ba6bf3939121aaa549a69f7
Kristian Dunne <KristianDunne@icloud.com>
2021-01-09 16:01:17 +0000

    Center the desktop layout

--- src/App.js
+++ src/App.js
@@ -3,21 +3,30 @@ import SignUpFrom from './components/SignUpForm';
 
 function App() {
   return (
-    <div className="flex flex-col h-screen px-5 py-20 text-center text-white app bg-primary-red">
-      <div className="px-10 text-2xl font-bold">
-        Learn to code by watching others
+    <div className="flex flex-col items-center h-screen px-5 py-20 lg:justify-center app bg-primary-red ">
+      <div
+        style={{ maxWidth: 1000 }}
+        className="flex flex-col items-center text-center text-white lg:flex-row lg:space-x-10"
+      >
+        <div className="flex-1 lg:text-left">
+          <div className="px-10 text-2xl font-bold lg:p-0">
+            Learn to code by watching others
+          </div>
+          <div className="mt-5 text-sm">
+            See how experienced developers solve problems in real-time. Watching
+            scripted tutorials is great, but understanding how developers think
+            is invaluable.
+          </div>
+        </div>
+        <div className="flex-1">
+          <div className="py-3 text-sm leading-loose text-center rounded-lg shadow-xl mt-14 price-alert bg-accent-blue">
+            <span className="text-sm font-bold">Try it free 7 days</span> then
+            <br />
+            $20/mo. thereafter
+          </div>
+          <SignUpFrom />
+        </div>
       </div>
-      <div className="mt-5 text-sm">
-        See how experienced developers solve problems in real-time. Watching
-        scripted tutorials is great, but understanding how developers think is
-        invaluable.
-      </div>
-      <div className="py-3 text-sm leading-loose text-center rounded-lg shadow-xl mt-14 price-alert bg-accent-blue">
-        <span className="text-sm font-bold">Try it free 7 days</span> then
-        <br />
-        $20/mo. thereafter
-      </div>
-      <SignUpFrom />
     </div>
   );
 }
